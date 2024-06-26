a2a385249c371a1adb7a1744cd62f786045d5d0d
虫子樱桃 <czyt.go@gmail.com>
2024-06-26 15:01:44 +0800

    use clap to parse args

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
-use std::env;
+use clap::Parser;
 use std::fs::{self, File};
 use std::io::{self, BufReader, BufWriter, Write};
 use std::path::Path;
 use regex::Regex;
 
+#[derive(Parser, Debug)]
+#[command(name = "merger")]
+#[command(version = "1.0")]
+#[command(author = "czyt <go.czyt@gmail.com>")]
+#[command(
+    about = "Merges holomotion device image files from an input directory into a single output file"
+)]
+struct Cli {
+    #[arg(short,long, default_value = ".",help = "the path with .part01,.part02 files")]
+    input_directory: String,
+    #[arg(short,long,help = "the path to save the combined img.xz file.for example:ubuntu-24.04.img.xz")]
+    output_file: String,
+}
+
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} <input_directory> <output_file>", args[0]);
-        return Ok(());
-    }
+    let args = Cli::parse();
+    let re = Regex::new(r"\d+").unwrap();
 
-    let input_dir = &args[1];
-    let output_file = &args[2];
-
-    let re = Regex::new(r"\d+").unwrap(); // Regex to match one or more digits
-
-    let mut entries: Vec<_> = fs::read_dir(input_dir)?
+    let mut entries: Vec<_> = fs::read_dir(&args.input_directory)?
         .filter_map(Result::ok)
         .map(|e| e.path())
         .filter(|p| {
@@ -34,16 +40,17 @@ fn main() -> io::Result<()> {
         num_part.parse::<u32>().unwrap()
     });
 
-    let output_path = Path::new(output_file);
+    let output_path = Path::new(&args.output_file);
     let output_file = File::create(&output_path)?;
     let mut writer = BufWriter::new(output_file);
 
-    for entry in entries {
+    entries.iter().try_for_each(|entry| -> io::Result<()> {
         println!("Merging {:?}", entry.file_name().unwrap());
         let input_file = File::open(entry)?;
         let mut reader = BufReader::new(input_file);
         io::copy(&mut reader, &mut writer)?;
-    }
+        Ok(())
+    })?;
     println!("Merge completed successfully!");
     Ok(())
 }
